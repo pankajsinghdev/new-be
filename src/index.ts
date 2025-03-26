81f72c81388378afa07b5d1b13f4eb6b4aae808d
pankaj <cohortpankaj@gmail.com>
2025-03-27 00:34:01 +0530

    cors add

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(cors());
 
 const prisma = new PrismaClient();
 
